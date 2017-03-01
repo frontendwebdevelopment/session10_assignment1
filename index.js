9c99a5eb91d072454533cfac3717eb38ba75e229
Ajay Kumar S M <ajaymandakki@gmail.com>
2017-03-01 16:40:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+var app = angular.module("myapp", []);
+app.controller("ListController", ['$scope', function($scope) {
+    $scope.personalDetails = [
+        {
+            'name':'Ajay',
+            'email':'ajay@acd.com'
+        },
+        {
+            'name':'Rahul',
+            'email':'rahul@acd.com'
+        },
+        {
+            'name':'Roy',
+            'email':'roy@roy.com'
+        }];
+    
+        $scope.addNew = function(personalDetails){
+            $scope.personalDetails.push({ 
+                'name': personalDetails.name, 
+                'email': personalDetails.email,
+            });
+            $scope.PD = {};
+        };
+    
+    
+}]);
